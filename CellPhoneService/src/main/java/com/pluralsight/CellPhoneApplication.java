7cd4a5f61488e209617c2b0377efe13d5803badc
Spati Dube <sdube@appdev.yearup.org>
2025-04-20 21:33:51 -0500

    "initial commit"

--- CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,8 +1,26 @@
 package com.pluralsight;
 
-public class CellPhoneApplicattion {
+public class CellPhoneApplication {
 
     public static void main(String[] args) {
+        CellPhone phone = new CellPhone(); // Create a new phone
+        phone.model = "myModel";
+        phone.carrier = "Tmobile";
+        phone.serialNumber = 12;
+        phone.owner = "Spati";
+        phone.phoneNumber = "214-214-214";
+
+        phone.displayInfo();
+
+        CellPhone newPhone = new CellPhone();
+        newPhone.model = "model 2.0";
+        newPhone.serialNumber = 3333;
+        newPhone.owner = "Dave";
+        newPhone.phoneNumber = ""
+
+
+
+
 
 
 
--- MatApp/src/main/java/com/pluralsight/MatApp.java
+++ MatApp/src/main/java/com/pluralsight/MatApp.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
 public class MatApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        double bobSalary = 50000;
+        double garySalary = 200000.99;
+
+        // then code solution
+        double highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("Highest salary: " + highestSalary);
+// ex: System.out.println("The answer is " + answer);
+// REPEAT FOR NEXT EXERCISE
+//MINIMUM CAR PRICE
+        double carPrice = 50000;
+        double truckPrice = 200000;
+
+        double lowestPrice =  Math.min(carPrice, truckPrice);
+
+        System.out.println("Lowest price: " + lowestPrice);
+
+// AREA OF CIRCLE THATS 7.25 Radius
+        double circleRadius = 7.25;
+        double circleArea = Math.PI * Math.pow(7.25, 2);
+
+        System.out.println("Circle area: " + circleArea);
+
+        // square root a variable that's 5.0
+
+        // distance between
+
+
+    }
+
 }
